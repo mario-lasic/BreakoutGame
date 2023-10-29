da29c8aa79581d56b8d21448e757a4b7d5caf016
Mario Lasić <mario.lasic@outlook.com>
2023-10-29 18:45:20 +0100

    Add collision detection

--- main.py
+++ main.py
@@ -23,5 +23,19 @@ while game_is_on:
     screen.update()
     time.sleep(ball.move_speed)
     ball.move()
+    if ball.xcor() > 480 or ball.xcor() < -480:
+        ball.bounce_x()
+    if ball.distance(main_paddle) < 80 and ball.ycor() < -340:
+        ball.bounce_y()
+    for brick in bricks.bricks:
+        difference = brick.ycor() - ball.ycor()
+        if ball.distance(brick) < 50 and difference in range(-20, 20):
+            print(f"{difference}")
+            brick.ht()
+            brick.goto(3000,3000)
+            bricks.bricks.remove(brick)
+            ball.bounce_y()
 
+    if len(bricks.bricks) == 0:
+        game_is_on = False
 screen.exitonclick()
